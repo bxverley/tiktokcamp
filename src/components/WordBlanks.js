c8af5efe707df230902f41dfd57e30a46d264891
Anteloped <ernest_lim@mymail.sutd.edu.sg>
2022-05-20 17:44:59 +0800

    ernest#

--- src/components/WordBlanks.js
+++ src/components/WordBlanks.js
@@ -1,12 +1,28 @@
-//import stuff you need here
-//Tasking No. 2
+import React, { Component } from 'react';
 
 export default function WordBlanks() {
-    //put any custom functions here
-  
-    return (
-      <div>
-          <h1> Your Guess: _ _ _ _ _ L</h1>
-      </div>
-    )
+  // var words = ["apple","juice","css","turtle"]; //demo wordbank
+
+  // let answer = "";
+  // let wordStatus = null;
+  // let guessed = [];
+
+  // function randomWord() {
+  //   answer = words[Math.floor(Math.random() * words.length)];
+  //   //alert(answer);
+  // }
+
+  // function guessedWord() {
+  //   wordStatus = answer.split('').map(letter => (guessed.indexOf(letter) >= 0 ? letter : " _ ")).join('');
+  //   document.getElementById('blanks').innerHTML = wordStatus //
+  // }
+
+  // randomWord();
+  // guessedWord();
+
+  return (
+    <div>
+        <p id="blanks">blanks</p>
+    </div>
+  )
 }
