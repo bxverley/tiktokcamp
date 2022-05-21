f97d0b6526472a8d5854bae18ad18224c0258205
chris-lye <christopher.zijian@gmail.com>
2022-05-21 14:31:40 +0800

    Update WrongWords

--- src/components/WrongWords.js
+++ src/components/WrongWords.js
@@ -1,13 +1,44 @@
 //import stuff you need here
 //Tasking No. 3
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 
 export default function WrongWords() {
     //put any custom functions here
+    let word = ['A', 'P', 'P', 'L', 'E'];
+
+    let wrongLetters = ['O','R','D']
+
+    const updateWrongLetter = letter => {
+        if (word.includes(letter) === false) {
+            wrongLetters.push(letter);
+        }
+        return wrongLetters
+    }
+
+
+    class StrikeThrough extends React.Component {
+        render() {
+            return (
+                <div>
+                    <h2 style={{textDecorationLine: 'line-through', textDecorationStyle: 'solid'}}>
+                    {wrongLetters}
+                    </h2>
+                </div>
+            )
+        }
+    }
   
     return (
       <div>
-          <h1> Words Guessed: O</h1>
+        <StrikeThrough />
       </div>
     )
 }
+
+//Text component does not exist, the render part was abit off
+
+
+
+
