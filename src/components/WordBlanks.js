e4ca4e0e1c07b34ef1469ce6305db56a748f63a7
chris-lye <christopher.zijian@gmail.com>
2022-05-21 22:17:49 +0800

    Update WordBlanks

--- src/components/WordBlanks.js
+++ src/components/WordBlanks.js
@@ -3,28 +3,29 @@
 import React, { Component } from 'react';
 
 export default function WordBlanks() {
-  // var words = ["apple","juice","css","turtle"]; //demo wordbank
+  var words = ["apple","juice","css","turtle"]; //demo wordbank
 
-  // let answer = "";
-  // let wordStatus = null;
-  // let guessed = [];
+  let answer = "";
+  let wordStatus = null;
+  let guessed = ["a", "j", "c", "t"];
 
-  // function randomWord() {
-  //   answer = words[Math.floor(Math.random() * words.length)];
-  //   //alert(answer);
-  // }
+  function randomWord() {
+    answer = words[Math.floor(Math.random() * words.length)];
+    //alert(answer);
+  }
 
-  // function guessedWord() {
-  //   wordStatus = answer.split('').map(letter => (guessed.indexOf(letter) >= 0 ? letter : " _ ")).join('');
-  //   document.getElementById('blanks').innerHTML = wordStatus //
-  // }
+  function guessedWord() {
+    wordStatus = answer.split('').map(letter => (guessed.indexOf(letter) >= 0 ? letter : " _ ")).join('');
+    // document.getElementById('blanks').innerHTML = wordStatus //
+  }
 
-  // randomWord();
-  // guessedWord();
+  randomWord();
+  guessedWord();
 
   return (
     <div>
-        <p id="blanks">blanks</p>
+        {/* <p id="blanks">blanks</p> */}
+        <p >{wordStatus}</p>
     </div>
   )
 }
