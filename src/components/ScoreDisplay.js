962f38bb0e3d35e8f004ea1f6d9013f601c3aa2d
chris-lye <christopher.zijian@gmail.com>
2022-05-28 21:08:50 +0800

    Add game over screen to App, fixed win counter

--- src/components/ScoreDisplay.js
+++ src/components/ScoreDisplay.js
@@ -1,12 +1,16 @@
 //import stuff you need here
 //Tasking No. 5 + put a wordbank file in src/
+import {useSelector, useDispatch} from 'react-redux';
 
 export default function ScoreDisplay() {
     //put any custom functions here
+
+    //Redux stuff
+    const hangman = useSelector(state => state.hangman);
   
     return (
       <div>
-          <h1>Current Score: </h1>
+          <h1>Current Score: {hangman.winCounter} </h1>
       </div>
     )
 }
